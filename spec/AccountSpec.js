90329b45b06eb38c3c89945855ed50bcb4799221
Peter Johnstone <peter.wdj@gmail.com>
2018-01-02 15:59:09 +0000

    Fix bug preventing beforeEach working; refactor to use beforeEach block

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,34 +1,29 @@
 'use strict';
-
 describe('Account', function() {
   var Account = require('../lib/account.js').Account;
   var account;
 
   beforeEach(function() {
-    var account = new Account();
+    account = new Account();
   });
 
   describe('#getBalance', function() {
     it('Should have a balance of 0', function() {
-      var account = new Account();
       expect(account.getBalance()).toEqual(0);
-    });
-  });
-
-  describe('#deposit', function() {
-    it('Should have a balance of 100', function() {
-      var account = new Account();
-      account.deposit(100);
-      expect(account.getBalance()).toEqual(100);
     });
   });
 
   describe('#setBalance', function() {
     it('Should increment an account\'s balance', function() {
-      var account = new Account();
       account.setBalance(50);
       expect(account.getBalance()).toEqual(50);
-    })
-  })
+    });
+  });
 
+  describe('#deposit', function() {
+    it('Should have a balance of 100', function() {
+      account.deposit(100);
+      expect(account.getBalance()).toEqual(100);
+    });
+  });
 });
