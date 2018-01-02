69c00171c1c613efd2efefdce759d65051946528
Peter Johnstone <peter.wdj@gmail.com>
2018-01-02 15:20:47 +0000

    Pass tests to set balance and make a deposit

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -8,8 +8,27 @@ describe('Account', function() {
     var account = new Account();
   });
 
-  it('Should have a balance of 0', function() {
-    var account = new Account();
-    expect(account.getBalance()).toEqual(0);
+  describe('#getBalance', function() {
+    it('Should have a balance of 0', function() {
+      var account = new Account();
+      expect(account.getBalance()).toEqual(0);
+    });
   });
+
+  describe('#deposit', function() {
+    it('Should have a balance of 100', function() {
+      var account = new Account();
+      account.deposit(100);
+      expect(account.getBalance()).toEqual(100);
+    });
+  });
+
+  describe('#setBalance', function() {
+    it('Should increment an account\'s balance', function() {
+      var account = new Account();
+      account.setBalance(50);
+      expect(account.getBalance()).toEqual(50);
+    })
+  })
+
 });
