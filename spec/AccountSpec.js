ebb1ebe76e765d5e8588efd7ea95a8c944f7477a
Peter Johnstone <peter.wdj@gmail.com>
2018-01-02 16:06:03 +0000

    Pass test to withdraw money

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -26,4 +26,12 @@ describe('Account', function() {
       expect(account.getBalance()).toEqual(100);
     });
   });
+
+  describe('#withdraw', function() {
+    it('Should have a balance of 50', function() {
+      account.deposit(100);
+      account.withdraw(50);
+      expect(account.getBalance()).toEqual(50);
+    })
+  })
 });
