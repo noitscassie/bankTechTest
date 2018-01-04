25ebd6aab803d6ad4cd3094ea1c647150736b163
Peter Johnstone <peter.wdj@gmail.com>
2018-01-04 10:16:55 +0000

    Add passing test for Withdrawal withdraw method

--- spec/WithdrawalSpec.js
+++ spec/WithdrawalSpec.js
@@ -1,12 +1,28 @@
 'use strict';
 
 describe('Withdrawal', function() {
+  var Account = require('../lib/account.js').Account;
   var Withdrawal = require('../lib/withdrawal.js').Withdrawal;
+  var account;
+  var withdrawal;
+
+  beforeEach(function() {
+    account = new Account(50);
+    withdrawal = new Withdrawal(account, 50);
+  });
 
   describe('Date', function() {
     it('Should be a date object', function() {
-      var withdrawal = new Withdrawal();
       expect(withdrawal.date).toEqual(jasmine.any(Date));
+    });
+  });
+
+  describe('#withdraw', function() {
+    it('Should reduce an account\'s balance', function() {
+      spyOnProperty(account, 'balance', 'get').and.returnValue(50);
+      var balanceSetterSpy = spyOnProperty(account, 'balance', 'set');
+      account.withdraw(50);
+      expect(balanceSetterSpy).toHaveBeenCalled();
     });
   });
 });
