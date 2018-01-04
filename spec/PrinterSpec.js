4cc5b8c55b364d81b175bb86cc16844388757eea
Peter Johnstone <peter.wdj@gmail.com>
2018-01-04 22:35:11 +0000

    Rewrite test with correct syntax

--- spec/PrinterSpec.js
+++ spec/PrinterSpec.js
@@ -7,7 +7,8 @@ describe('Printer', function() {
     it('Should format a date to DD/MM/YYYY', function() {
       var date = new Date(2017, 6, 3);
       var printer = new Printer();
-      expect(function() { printer.formateDate(date) }).toEqual('03/07/2017');
+      var formattedDate = printer.formatDate(date);
+      expect(formattedDate).toEqual('03/07/2017');
     });
   });
 });
